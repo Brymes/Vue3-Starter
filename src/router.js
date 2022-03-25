aff85dc002917d9b617edb342cc5fd13b2a8278e
Timothy Odebunmi <dev.brymes@gmail.com>
2022-03-25 05:09:18 +0100

    Vue-Router

---
+++ src/router.js
@@ -0,0 +1,11 @@
+import { createWebHistory, createRouter } from "vue-router";
+// Import Componenets Here
+
+const routes = []
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
